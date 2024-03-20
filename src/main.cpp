e9aaf95659d6f38b548f5daf2ae7d7d3bf879027
Tiago Petena <tiagopetena@gmail.com>
2024-03-20 16:38:50 -0300

    added new classes to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
+#include "color.hpp"
+
 #include <iostream>
-#include <ostream>
-
-
 
 int main() {
 
@@ -17,16 +16,13 @@ int main() {
     for(int j = 0; j < image_height; ++j) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << std::flush;
         for (int i = 0; i < image_width; ++i) {
-        
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0;
+            auto pixel_color = color(
+                    double(i)/(image_width-1),
+                    double(j)/(image_height-1),
+                    0);
+            write_color(std::cout, pixel_color);
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
 
