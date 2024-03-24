fef4b264daa3ab922f8c07c743aa44e27628398d
Tiago Petena <tiagopetena@gmail.com>
2024-03-24 12:04:23 -0300

    added labels to each vec3 operation

--- src/vec3.hpp
+++ src/vec3.hpp
@@ -56,19 +56,19 @@ using point3 = vec3;
 inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
-
+// Sum (u + v)
 inline vec3 operator+(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
-
+// Subtraction (u - v)
 inline vec3 operator-(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
-
+// Multiplication elementwise (u * v)
 inline vec3 operator*(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
-
+// Scalar multiplication
 inline vec3 operator*(double t, const vec3 &v) {
     return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
@@ -76,17 +76,17 @@ inline vec3 operator*(double t, const vec3 &v) {
 inline vec3 operator*(const vec3 &v, double t) {
     return t * v;
 }
-
+// Scalar division
 inline vec3 operator/(vec3 v, double t) {
     return (1/t) * v;
 }
-
+// Dot product (u . v)
 inline double dot(const vec3 &u, const vec3 &v) {
     return u.e[0] * v.e[0]
         + u.e[1] * v.e[1]
         + u.e[2] * v.e[2];
 }
-
+// Cross product (u x v)
 inline vec3 cross(const vec3 &u, const vec3 &v) {
     return vec3(
             u.e[1] * v.e[2] - u.e[2] * v.e[1],
