88efbe52324c618945aa533ea2d35d5389749f43
Tiago Petena <tiagopetena@gmail.com>
2024-03-27 15:38:21 -0300

    added gamma correction

--- src/color.hpp
+++ src/color.hpp
@@ -9,6 +9,10 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+    return pow(linear_component, 1/2.2);
+}
+
 
 void write_color(std::ostream &out, color pixel_color, int spp) {
     auto r = pixel_color.x();
@@ -19,6 +23,11 @@ void write_color(std::ostream &out, color pixel_color, int spp) {
     r *= scale;
     g *= scale;
     b *= scale;
+
+    // Gamma correction
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Write the translated [0, 255] value  to each color component
     static const interval intensity(0.000, 0.999);
